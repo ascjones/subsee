44d7990423432796572b7e968336708d81e33cee
Andrew Jones <ascjones@gmail.com>
2021-01-18 09:47:31 +0000

    Add option to specify the substrate node url

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ CLI to inspect substrate metadata.
 
 `cargo install --git https://github.com/ascjones/subsee`
 
+```
+Usage: subsee [--url <url>]
+
+Inspect substrate metadata
+
+Options:
+  --url             the url of the substrate node to query for metadata
+  --help            display usage information
+```
+
 Currently just outputs the metadata of a substrate node as json.
 
 Expects a substrate node to be running locally with the default endpoint `"http://localhost:9933"`.
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,42 @@
-use color_eyre::eyre::eyre;
+use argh::FromArgs;
+use color_eyre::eyre::{self, WrapErr};
 use frame_metadata::v12::RuntimeMetadataPrefixed;
+
+#[derive(FromArgs)]
+/// Inspect substrate metadata
+struct SubSee {
+    /// the url of the substrate node to query for metadata
+    #[argh(option, default = "String::from(\"http://localhost:9933\")")]
+    url: String,
+}
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
-    // todo: make CLI args
-    let url = "http://localhost:9933";
+    let args: SubSee = argh::from_env();
 
-    let metadata = fetch_metadata(&url)?;
-    let decoded: RuntimeMetadataPrefixed<String> = scale::Decode::decode(&mut &metadata[..])?;
-    let serialized = serde_json::to_string_pretty(&decoded)?;
+    let metadata = fetch_metadata(&args.url)?;
+    let serialized = serde_json::to_string_pretty(&metadata)?;
+
     println!("{}", serialized);
-
     Ok(())
 }
 
-fn fetch_metadata(url: &str) -> color_eyre::Result<Vec<u8>> {
+fn fetch_metadata(url: &str) -> color_eyre::Result<RuntimeMetadataPrefixed<String>> {
     let resp = ureq::post(url)
         .set("Content-Type", "application/json")
         .send_json(ureq::json!({
-              "jsonrpc": "2.0",
-              "method": "state_getMetadata",
-              "id": 1
-          }))?;
+            "jsonrpc": "2.0",
+            "method": "state_getMetadata",
+            "id": 1
+        }))
+        .context("error fetching metadata from the substrate node")?;
+
     let json: serde_json::Value = resp.into_json()?;
     let hex_data = json["result"]
         .as_str()
-        .ok_or(eyre!("metadata result field should be a string"))?;
+        .ok_or(eyre::eyre!("metadata result field should be a string"))?;
+
     let bytes = hex::decode(hex_data.trim_start_matches("0x"))?;
-    Ok(bytes)
+    let decoded = scale::Decode::decode(&mut &bytes[..])?;
+    Ok(decoded)
 }
